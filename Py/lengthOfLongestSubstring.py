3224e4fc866d4f3f5692d1b15eaab6370ba8353d
eliasdagger <125106050+eliasdagger@users.noreply.github.com>
2026-07-25 16:26:17 -0400

    digitFrequencyScore, note explanations

--- Py/lengthOfLongestSubstring.py
+++ Py/lengthOfLongestSubstring.py
@@ -11,3 +11,20 @@ class Solution(object):
             res = max(res, r - l)
 
         return res
+
+# O(n) solution which uses a hash set to dictate l and r's position like an accordian, r will continue to extend at an increment of 1, updating count (c) if a new longest unique substring is found, if we find a duplicate, l will clear the hash set one by one removing the element of its index, this will continue until 
+# duplicate is out of the hashset, then r will continue its path. This is done because we count the lenght of a substring, then when we encounter a duplicate we know that the substring from l - r is counted we can reframe until we remove the duplicate
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        char_set = set()
+        l, c = 0, 0
+
+        for r in range(len(s)):
+            while s[r] in char_set:
+                char_set.remove(s[l])
+                l += 1
+
+            char_set.add(s[r])
+            c = max(c, r - l + 1)
+        
+        return c
--- Py/numberOfEmployeesWhoMetTarget.py
+++ Py/numberOfEmployeesWhoMetTarget.py
@@ -1,11 +1,6 @@
-class Solution(object):
-    def numberOfEmployeesWhoMetTarget(self, hours, target):
-        # count = the sum for each i in hours such that hours[i] >= target, each employee which meets this condition will increment count by 1
-        count = sum(1 for i in hours if i >= target)
-        return count
-        """
-        :type hours: List[int]
-        :type target: int
-        :rtype: int
-        """
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        # sum (1 will be added, for x in nums such that IF x >= target) the sum will sum all true employees that are above the target
+        res = sum(1 for x in hours if x >= target)
+        return res
         
