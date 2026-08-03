7005c0cdad4b4ddaa831652f7f615d1f7cd96847
eliasdagger <125106050+eliasdagger@users.noreply.github.com>
2026-08-03 14:56:56 -0400

    explanation

--- Py/checkInconclusion.py
+++ Py/checkInconclusion.py
@@ -1,5 +1,6 @@
 
 def checkInclusion(s1: str, s2: str) -> bool:
+    # if the sorted version of s1 is in our sorted window, return true, continue until window is the entire length of s2, then return false if no permutation is found. 
     l, r = 0, len(s1)
     while r <= len(s2):
         if "".join(sorted(s1)) in "".join(sorted(s2[l:r])):
@@ -8,9 +9,4 @@ def checkInclusion(s1: str, s2: str) -> bool:
             r += 1
             l += 1
     return False
-
-s1 = "abc"
-s2 = "lecabee"
-
-print(checkInclusion(s1, s2))
 
