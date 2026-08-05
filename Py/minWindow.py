da3606fdde7fb7b41aaa805bdba726e5e404a305
eliasdagger <125106050+eliasdagger@users.noreply.github.com>
2026-08-05 11:09:58 -0400

    minwindow

--- Py/minWindow.py
+++ Py/minWindow.py
@@ -1,42 +1,39 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if t == "": return ""
+        # create left and right pointers, every number is less than infinity so comparing to inf will readjust minimum substring
+        res, resLen = [-1,-1], float("infinity")
+        l = 0
+        # Create two dcts, here we can count frequency of the chars of t and if they match to s substring
+        countT, window = {}, {}
 
+        # count frequencies
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+        
+        # we need to find len(countT) in our window, this counts the unique keys in the dct
+        have, need = 0, len(countT)
 
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
 
-def minWindows(s: str, t: str) -> str:
-    # base case: if s is shorter than t, its impossible for substring to exist
-    if len(s) < len(t):
-        return ""
+            # we want to have 
+            if c in countT and window[c] == countT[c]:
+                have += 1
 
-    res = {}
-    count1 = {}
-    # Sliding window technique, set two pointers left and right
-    l, r = 0, 0
+            while have == need:
+                if (r - l + 1) < resLen:
+                    # readjust res
+                    res = [l, r]
+                    resLen = r - l + 1
 
-    # create a hash table of chars in t mapped to their frequency (handles duplicates). 
-    for c in t:
-        count1[c] = 1 + count1.get(c, 0)
+                # removing a char from the window while readjusting the window, decrement the counter, if this was a "need" char, and now the window has less than we "need" decrement have and shift left pointer. 
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
 
-    need, have = len(count1), 0
-
-    while r < len(s):
-        if s[l] not in count1:
-            l += 1
-            r += 1
-            continue
-
-        if 
-
-
-
-    
-
-    
-
-    
-
-    
-
-    
-    
-    while r < len(s):
-
-print(minWindow())
+                l += 1
+        # if the string hasnt been changed != inf, then return our pointers else, it doesnt exist thus return ""
+        l, r = res
+        return s[l:r+1] if resLen != float("infinity") else ""
