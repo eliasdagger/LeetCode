53cc1eb8802b6f921d9f2910cae18f51337f524e
Elie Dagher <125106050+eliasdagger@users.noreply.github.com>
2026-08-21 19:15:24 -0400

    hasCycle explanation and in py

--- Java/hasCycle.java
+++ Java/hasCycle.java
@@ -12,7 +12,9 @@
  * Example: 3->2->0->-4 where the last node points back at 2  ->  true
  */
 
-public class Solution {
+public class hasCycle {
+    // create a fast and slow pointer, the algo will be if they ever == there is a loop since they will land on the same node at one point
+    // use fast as the conditions since it will reach null first if there is no loop, if there is no loop fast wont == slow and while loop will break thus returning false
     public boolean hasCycle(ListNode head) {
         ListNode fast, slow;
         fast = slow = head;
@@ -31,3 +33,20 @@ public class Solution {
         return false;
     }
 }
+
+// class Solution:
+//     def hasCycle(self, head: Optional[ListNode]) -> bool:
+//         slow = head
+//         fast = head
+
+//         while fast is not None:
+//             if fast.next is None:
+//                 return False
+
+//             slow = slow.next
+//             fast = fast.next.next
+
+//             if slow == fast:
+//                 return True
+
+//         return False
