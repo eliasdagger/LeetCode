f4baa880624e454311a1f2763bc3ff797a5df589
Elie Dagher <125106050+eliasdagger@users.noreply.github.com>
2026-08-27 19:34:51 -0400

    search

--- Py/search.py
+++ Py/search.py
@@ -11,6 +11,10 @@
 
 
 def search(nums: List[int], target: int) -> int:
+    # Set pointers to either end of our sorted array, if l <= r create our midpoint to be the window from l->r // 2 which will be half the length of our current range 
+    # then + l, to make the mid point of the list be l + half the len(window). we want to move bounds to close our window and narrow down to a solution, 
+    # so, if mid val is < target meaning target is on the right of mid, change the bounds where l = mid, then add 1 since we know mid != target. Similarily for r pointer
+    # if mid index == target return mid (index).  
     l, r = 0, len(nums) - 1
     while l <= r:
         m = l + ((r - l) // 2)
